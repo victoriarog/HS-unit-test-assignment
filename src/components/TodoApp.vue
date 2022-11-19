6e7d1a977d161c051e1c53baa7ea9159d7735d36
Dani Nebot <daniign@gmail.com>
2022-11-20 00:32:04 +0100

    Initial commit

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+
+const todos = ref([
+  {
+    id: 1,
+    text: 'Learn Vue.js 3',
+    completed: false,
+  },
+]);
+</script>
+<template>
+  <div></div>
+</template>
---
+++ src/components/__tests__/TodoApp.spec.js
@@ -0,0 +1,11 @@
+import { mount } from '@vue/test-utils';
+import { expect, test } from 'vitest';
+import TodoApp from '../TodoApp.vue';
+
+test('renders a todo', () => {
+  const wrapper = mount(TodoApp);
+
+  const todo = wrapper.get('[data-test="todo"]');
+
+  expect(todo.text()).toBe('Learn Vue.js 3');
+});
